af0494ac1c0f70aa68dab82228f8556d6ee995df
Irina Kuzema <irina.kuzema@duniceedge.com>
2017-04-05 15:58:51 +0300

    Add paginate

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,35 +2,35 @@ class TodosController < ApplicationController
 
   def index
     if params[:filter]
-      @todos = Todo.where(complete: params[:filter]).order(id: :desc)
+      @todos = Todo.where(complete: params[:filter]).order(id: :desc).paginate(page: params[:page], per_page: 10)
     else
-      @todos = Todo.all.order(id: :desc)
+      @todos = Todo.all.order(id: :desc).paginate(page: params[:page], per_page: 10)
     end
   end
 
   def create
     @todo = Todo.new(todos_params)
     @todo.save
-    redirect_to root_path
+    redirect_to(:back)
   end
 
 
   def update
     @todo = Todo.find_by(id: params[:id])
     @todo.update_attributes(todos_params)
-    redirect_to root_path
+    redirect_to(:back)
   end
 
   def destroy
     @todo = Todo.find_by(id: params[:id])
     @todo.destroy
-    redirect_to root_path
+    redirect_to(:back)
   end
 
   def destroy_completed
     @todos = Todo.where(complete: true)
     @todos.destroy_all
-    redirect_to root_path
+    redirect_to(:back)
   end
 
   private
